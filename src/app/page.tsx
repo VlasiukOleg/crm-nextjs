8dd2ffa3bb2d9f40caaabfdb99d24411481cf09b
Oleg <ikslprint@gmail.com>
2024-04-16 16:52:17 +0300

    add bg color to global styles, addactive label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import ActiveLabel from './components/active-label';
+
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <ActiveLabel>Active</ActiveLabel>
     </main>
   );
 }
